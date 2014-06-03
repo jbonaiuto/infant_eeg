73a00966a6978895ec9ff6bbaa6181b6a3fc64e3
jbonaiuto <jbonaiuto@gmail.com>
2014-06-03 11:22:30 +0100

    initial import

---
+++ src/sh/infantEEG.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET PYTHONPATH=D:\Sources\Python\broadmonkController\src\python
+SET PYGLET_SHADOW_WINDOW=0
+D:
+cd \Sources\Python\broadmonkController\src\python
+python broadmonkController\main.py
