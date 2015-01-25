6b71c5f45847d0d54f64b23bf5cca77f68fd48d3
James Bonaiuto <jbonaiuto@gmail.com>
2015-01-25 14:13:36 +0000

    integrate videos with distractors

--- src/python/infant_eeg/distractors.py
+++ src/python/infant_eeg/distractors.py
@@ -2,18 +2,16 @@ import numpy as np
 import os
 from psychopy import visual, sound, event
 
-__author__ = 'jbonaiuto'
-
-
 class DistractorSet:
     """
     A set of distractor images and sounds
     """
 
-    def __init__(self, image_path, sound_path, reward_image_file, duration_frames, win):
+    def __init__(self, image_path, sound_path, video_path, reward_image_file, duration_frames, win):
         """
         image_path - path to load distractor images from
         sound_path - path to load distractor sounds from
+        video_path - path to load distractor videos from
         reward_image_file - file containing reward image
         duration_frames - duration of each distractor image in frames
         win - window to display images in
@@ -31,6 +29,9 @@ class DistractorSet:
         sound_files = [f for f in os.listdir(sound_path) if os.path.isfile(os.path.join(sound_path, f))]
         for f in sound_files:
             self.sounds.append(sound.Sound(os.path.join(sound_path, f)))
+
+        # find videos
+        self.video_files = [os.path.join(video_path,f) for f in os.listdir(video_path) if os.path.isfile(os.path.join(video_path, f))]
 
         # load reward image
         self.reward_image = visual.ImageStim(win, reward_image_file)
@@ -76,3 +77,22 @@ class DistractorSet:
                 self.reward_image.draw()
                 self.win.flip()
                 all_keys = event.getKeys()
+        elif this_key == 'V':
+            self.show_video()
+
+    def show_video(self):
+        # clear any keystrokes before starting
+        event.clearEvents()
+        all_keys = []
+
+        # Pick a video at random
+        video_idx=np.random.choice(range(len(self.video_files)))
+        video = visual.MovieStim(self.win, self.video_files[video_idx], size=[1280, 1024])
+
+        # wait for a keypress
+        while len(all_keys) == 0 and not video.status==visual.FINISHED:
+            video.draw()
+            self.win.flip()
+            # Look for key press
+            all_keys = event.getKeys()
+        video.stop()
